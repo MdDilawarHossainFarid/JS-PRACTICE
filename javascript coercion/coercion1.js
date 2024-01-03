9db9fcc5a5661c1ac46e151d3843840737003b8d
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-01-03 22:45:21 +0530

    some example

--- javascript coercion/coercion1.js
+++ javascript coercion/coercion1.js
@@ -1,3 +1,9 @@
 console.log( 2 - 7);
 console.log( 2 - null);
 console.log( 2 - undefined);
+console.log(10 - true);
+console.log(10 - false);
+console.log(10 - "50");
+console.log(10 - "50abcd");
+console.log(1 - "0xa");
+
