6d8099adfb58681ebb18379d653a7b33f410009e
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-01-22 22:46:52 +0530

    consumingPromise

--- javascript coercion/coercion1.js
+++ javascript coercion/coercion1.js
@@ -1,9 +1,16 @@
-console.log( 2 - 7);
-console.log( 2 - null);
-console.log( 2 - undefined);
-console.log(10 - true);
-console.log(10 - false);
-console.log(10 - "50");
-console.log(10 - "50abcd");
-console.log(1 - "0xa");
+// console.log( 2 - 7);
+// console.log( 2 - null);
+// console.log( 2 - undefined);
+// console.log(10 - true);
+// console.log(10 - false);
+// console.log(10 - "50");
+// console.log(10 - "50abcd");
+// console.log(1 - "0xa");
+// console.log(undefined / null); when ever you put invalid situation  put NaN
+// IN JS ONLY NAN IS NOT EQUAL TO ITSELF
 
+// Undefined - something not define yet but may be define later
+
+// Null - It actually represents empty value
+
+console.log(typeof null);
