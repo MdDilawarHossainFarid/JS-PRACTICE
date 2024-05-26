3945f1dbee6dcb1ffdda71be3abb4f416f92f1a9
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-05-26 09:00:06 +0530

    ToNumber Abstract operation

--- Coercion/AbstractOperation/ToNumber.js
+++ Coercion/AbstractOperation/ToNumber.js
@@ -1,5 +1,10 @@
 console.log(10 - 5);
 console.log(10 - null);
 console.log(10 - undefined);
+
 console.log(10 - true);
 console.log(10 - false);
+console.log(10 - "111");
+
+console.log(10 - "1abcd11");
+console.log(10 - "0xa");
