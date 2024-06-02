be374add50fac2b8286accf5d647e8240495bbe7
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-06-02 09:33:07 +0530

    toString Abstract operation

--- Coercion/toPrimitive.js
+++ Coercion/toPrimitive.js
@@ -1,3 +1,24 @@
 let obj = {}
 
-console.log(10 - obj); //obj.valueof  --> object , toString --> [object object]
+// console.log(10 - obj); //obj.valueof  --> object , toString --> [object object]
+
+let obj1 = {a : 50, valueOf(){
+    return 2;
+}}
+
+// console.log(4 - obj1);
+
+let obj3 = {a:50, toString(){
+    return "10"
+}};
+
+console.log(30 - obj3);
+
+let  obj4 = {
+    b: 40,
+    toString(){
+        return {}
+    }
+}
+
+console.log(100 - obj4);
