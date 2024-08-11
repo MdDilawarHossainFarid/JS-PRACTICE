b34b5b5d8326e212b227bea67db5c52ce46632f5
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-08-11 21:31:30 +0530

    RegExpObject

---
+++ regExp/reg.js
@@ -0,0 +1,6 @@
+// let pattern = /Hello World/g;
+// let text = pattern.toString();
+
+let text = "The best things in life are free"; let pattern = /free/;
+let result = pattern.test(text);
+console.log(result)
